acbbb698e48258252a656255390a9d0706dc3693
joao paulo <joaopaulod@gmail.com>
2016-05-06 00:13:54 -0300

    Atualização do controler Solicitação

--- HelpServer/HelpServer/Controllers/SolicitacaoController.cs
+++ HelpServer/HelpServer/Controllers/SolicitacaoController.cs
@@ -10,29 +10,38 @@ namespace HelpServer.Controllers
 {
     public class SolicitacaoController : ApiController
     {
+        public ICollection<Solicitacao> solicitacoes { get; set; }
+
+        public SolicitacaoController()
+        {
+            solicitacoes = new List<Solicitacao>();
+            solicitacoes.Add(new Solicitacao { id = 1, solicitante = "joao paulo", andar = "3", sala = "314", criticidade = "alta", data = DateTime.Now, descricao = "Problema numero 1", responsavel = "guilherme" });
+            solicitacoes.Add(new Solicitacao { id = 2, solicitante = "Mayara", andar = "2", sala = "213", criticidade = "media", data = DateTime.Now, descricao = "Problema numero 2", responsavel = "ingrid" });
+            solicitacoes.Add(new Solicitacao { id = 3, solicitante = "guilherme", andar = "3", sala = "350", criticidade = "baixa", data = DateTime.Now, descricao = "Problema numero 3", responsavel = "peterson" });
+        }
+        
+
         // GET: api/Solicitacao
         public ICollection<Solicitacao> Get()
         {
             //todo: implementar busca da lista de solicitações
-            ICollection<Solicitacao> solicitacoes = new List<Solicitacao>();
-            Solicitacao teste = new Solicitacao();
-            solicitacoes.Add(new Solicitacao { id = 1, solicitante = "joao paulo", andar = "3", sala = "314", criticidade = "alta", data = DateTime.Now, descricao = "Problema numero 1", responsavel = "guilherme" });
-            solicitacoes.Add(new Solicitacao { id = 1, solicitante = "Mayara", andar = "2", sala = "213", criticidade = "media", data = DateTime.Now, descricao = "Problema numero 2", responsavel = "ingrid" });
-            solicitacoes.Add(new Solicitacao { id = 1, solicitante = "guilherme", andar = "3", sala = "350", criticidade = "baixa", data = DateTime.Now, descricao = "Problema numero 3", responsavel = "peterson" });
+            
             return solicitacoes;
         }
 
         // GET: api/Solicitacao/5
-        public string Get(int id)
+        public IEnumerable<Solicitacao> Get(long id)
         {
             //todo: implementar busca de solicitação por id
-            return "value";
+            return solicitacoes.Where(x => x.id == id);
         }
 
         // POST: api/Solicitacao
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Solicitacao value)
         {
             //todo: implementar cadastro de solicitação no sistema
+            solicitacoes.Add(value);
+
         }
 
         // PUT: api/Solicitacao/5
@@ -45,6 +54,7 @@ namespace HelpServer.Controllers
         public void Delete(int id)
         {
             //todo: implementar exclusão de solicitação
+            solicitacoes.Remove(solicitacoes.Where(x => x.id == id).First());
         }
     }
 }
